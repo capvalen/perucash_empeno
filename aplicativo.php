d0263109fd18cd1a072bcf943e7c3d238a99b92e
Infocat <infocat2.0@gmail.com>
2017-10-12 12:56:30 -0500

    2.6.1

--- aplicativo.php
+++ aplicativo.php
@@ -14,7 +14,7 @@ if(isset($_SESSION['Atiende'])){?>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="shortcut icon" href="images/favicon.png">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap-datepicker3.css">
-	<link rel="stylesheet" type="text/css" href="css/anatsunamun.css?version=2.0.10">
+	<link rel="stylesheet" type="text/css" href="css/anatsunamun.css?version=2.0.14">
 	<link rel="stylesheet" type="text/css" href="css/icofont.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap-table.css">
 </head>
